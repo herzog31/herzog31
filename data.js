1ab57a280241f7fe83e137decc02ff4bc7040f67
Mark J. Becker <herzog31@users.noreply.github.com>
2023-02-27 18:27:35 +0100

    Add reference data

---
+++ data.js
@@ -0,0 +1,40 @@
+{
+    "sections": [{
+        "name": "Blog Posts",
+        "items": [{
+            "title": "Introducing Adobe Experience Manager’s New Headless Extension for Adobe Commerce PWA Studio",
+            "link": "https://blog.developer.adobe.com/introducing-adobe-experience-managers-new-headless-extension-for-adobe-commerce-pwa-studio-12a0d6c5a4e9",
+            "date": 1623276000,
+            "thumbnail": "https://miro.medium.com/max/1200/1*HNCFQF4zq9DR8zdi74Jlrw.jpeg"
+        },
+        {
+            "title": "Create a Delightfully Shoppable Experience with the New WKND Reference Site and AEM’s Commerce Integration Framework",
+            "link": "https://blog.developer.adobe.com/learn-how-to-create-a-shoppable-experience-with-the-new-wknd-reference-site-and-cif-b3b2c161f67e",
+            "date": 1655935200,
+            "thumbnail": "https://miro.medium.com/max/1200/1*er5Bq4BHmty9VS9Ux55JYw.jpeg"
+        }]
+    },
+    {
+        "name": "Talks",
+        "items": [{
+            "title": "How to Build Content and Commerce Experiences Using App Builder and API Mesh",
+            "link": "https://www.youtube.com/watch?v=1JKYmB3J8Kc"
+        },
+        {
+            "title": "Commerce Integration Framework Extensibility via GraphQL",
+            "link": "https://www.youtube.com/watch?v=sOVtXt-xx4c"
+        },
+        {
+            "title": "GraphQL services in the AEM world",
+            "link": "https://www.youtube.com/watch?v=8v-ZkLZCYRQ"
+        },
+        {
+            "title": "Enrich Commerce Experiences with Associated Content",
+            "link": "https://experienceleague.adobe.com/docs/adobe-developers-live-events/events/2021/oct2021/associated-content.html?lang=en"
+        },
+        {
+            "title": "Experience Manager Extension for PWA Studio",
+            "link": "https://experienceleague.adobe.com/docs/adobe-developers-live-events/events/2021/oct2021/aem-pwa-studio.html?lang=en"
+        }]
+    }]
+}
